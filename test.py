a3b0bd26caf865d23319cd425ae5b7cc1a68c78d
daviddcbud <daviddcbud@gmail.com>
2013-06-02 14:42:25 -0500

    another

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import sys
-data={'age':'3', 'name':'david'}
-print(data, end=",", file=sys.stderr)
-print('testing')
 
+def CtoF(c):
+    return 32.0 + ((212.0 - 32.0)/100) * c
+
+list=[1,2,3]
+import random
+print(random.choice(list))
+ 
