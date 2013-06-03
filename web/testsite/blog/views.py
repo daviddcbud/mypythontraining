1979e2b2dd5d5d3aa504f75b0746b63293662793
daviddcbud <daviddcbud@gmail.com>
2013-06-02 22:07:37 -0500

    nightly

--- web/testsite/blog/views.py
+++ web/testsite/blog/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.shortcuts import render,get_object_or_404
+from blog.models import Entry,Blog
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+def index(request):
+    list=Entry.objects.all()
+    return render(request,'blog/index.html',{'data':list})
+
+def detail(request, id):
+    entry=get_object_or_404(Entry,pk=id)
+    return render(request, 'blog/detail.html', {'data':entry})
+
+def update(request,id):
+    entry=get_object_or_404(Entry,pk=id)
+    entry.text=request.POST['text']
+    entry.save()
+    return HttpResponseRedirect(reverse('blog:index' ))
+    
